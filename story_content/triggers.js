d73342187243bbfc0bb12fe01030552b08e8d9f0
Tanvi1298 <tanvijain1298@gmail.com>
2024-05-31 18:29:20 +0530

    testing

--- story_content/triggers.js
+++ story_content/triggers.js
@@ -2,107 +2,14 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6gqH6Q1FNXP":
+      case "6YhidWuJ6oM":
         Script1();
         break;
-      case "6ayuCkl3mWo":
+      case "6lQgA56jkes":
         Script2();
         break;
-      case "6Osq2lnMl5j":
+      case "5b07pA8Osnx":
         Script3();
-        break;
-      case "5rKg0YPSqIL":
-        Script4();
-        break;
-      case "6YOaZxxKgJW":
-        Script5();
-        break;
-      case "6ScQEUjmHFu":
-        Script6();
-        break;
-      case "6WhBYJr2JvC":
-        Script7();
-        break;
-      case "6cYkkGpoFX6":
-        Script8();
-        break;
-      case "69a2GdwkxSQ":
-        Script9();
-        break;
-      case "6l0UqzojIst":
-        Script10();
-        break;
-      case "6an0KqUFrcd":
-        Script11();
-        break;
-      case "5hCeQx6RDVc":
-        Script12();
-        break;
-      case "6E74N35lcXt":
-        Script13();
-        break;
-      case "5Yz8HsAA5jl":
-        Script14();
-        break;
-      case "5YiO9aRBHIK":
-        Script15();
-        break;
-      case "62n54c25cgx":
-        Script16();
-        break;
-      case "61dcnPke3vT":
-        Script17();
-        break;
-      case "6oI3vQpixVx":
-        Script18();
-        break;
-      case "6SKDNKQ15cF":
-        Script19();
-        break;
-      case "5sCdv96PypB":
-        Script20();
-        break;
-      case "6I1PyzAvd9u":
-        Script21();
-        break;
-      case "6qShbyIZvLI":
-        Script22();
-        break;
-      case "6Jnh9Yd3unx":
-        Script23();
-        break;
-      case "68QutuGxAgD":
-        Script24();
-        break;
-      case "5u0V8h1GHXq":
-        Script25();
-        break;
-      case "62aqKtAW2pW":
-        Script26();
-        break;
-      case "68UwTwX4uOH":
-        Script27();
-        break;
-      case "6iqJe32KMyK":
-        Script28();
-        break;
-      case "6VLq68l6i8M":
-        Script29();
-        break;
-      case "6A85lZZPY89":
-        Script30();
-        break;
-      case "6OXe4gXrwHH":
-        Script31();
-        break;
-      case "6noVmweqDra":
-        Script32();
-        break;
-      case "5uYUm1SPeoO":
-        Script33();
-        break;
-      case "5f5crlUOUrg":
-        Script34();
         break;
   }
 }
